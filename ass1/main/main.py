1b64dc02dee68965b3a14465e2832964db331f49
AndreasDahl <andreas@hrdahl.dk>
2015-12-13 01:06:44 +0100

    good enough for re-submission

--- ass1/main/main.py
+++ ass1/main/main.py
@@ -7,14 +7,36 @@ colours = ['red', 'yellow', 'green']
 
 
 @process
-def producer(chan_out):
-    for _ in range(10):
-        chan_out(random.choice(colours))
-    retire(chan_out)
+def producer(chan_out, pause_chan):
+    ''' Producer and camera
+    '''
+    pauses = 0
+    try:
+        # Every iteration in this loop is a "tick" for the whole application
+        while True:
+            # Asyncronusly see if the pause channel has any messages
+            c, msg = PriSelect(InputGuard(pause_chan), SkipGuard())
+            if msg is not None:
+                pauses = msg
+            if pauses <= 0:
+                # Produce a ball and send it to the conveyor
+                chan_out(random.choice(colours))
+            else:
+                # Do not produce a ball on paused ticks, but decrement the pause counter
+                pauses -= 1
+
+    except ChannelRetireException:
+        print "producer stopping"
 
 
 @process
 def conveyor(chan_in, basket_chan):
+    ''' Conveyor belt, advances the balls and makes sure they are moved to the
+        correct basket
+
+        TODO: Support multiple baskets
+        TODO: More flexible belt length
+    '''
     places = [None] * 3
 
     try:
@@ -32,16 +54,26 @@ def conveyor(chan_in, basket_chan):
             places[2] = None
     except ChannelRetireException:
         print "conveyor: %s" % places
+        retire(chan_in)
         retire(basket_chan)
 
 
+
 @process
-def basket(chan_in):
+def basket(chan_in, pause_chan, capacity):
     count = 0
     while True:
         ball = chan_in()
-        print ball
-        count += 1
+        if ball is not None:
+            print ball
+            count += 1
+            if count >= capacity:
+                count = 0
+                pause_chan(5)  # notify number of ticks the basket require to empty itself
+    # The basket should not reach this point
+    print "BASKET RETIRE"
+    retire(chan_in)
+    retire(pause_chan)
 
 
 @process
@@ -51,14 +83,16 @@ def camera(chan_in, chan_out):
 
 
 if __name__ == '__main__':
+    print "run"
     producerChan = Channel("producer")
     basketChan = Channel("basket")
+    pauseChan = Channel("pause")
+    capacities = 10
 
     Parallel(
-        basket(basketChan.reader()),
+        basket(basketChan.reader(), pauseChan.writer(), capacities),
         conveyor(producerChan.reader(), basketChan.writer()),
-        producer(producerChan.writer())
+        producer(producerChan.writer(), pauseChan.reader())
     )
 
     shutdown()
-
