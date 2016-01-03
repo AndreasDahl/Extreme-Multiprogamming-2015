3e0a12fc90b6c4ada2cd2d65ca341cd92c5e6011
AndreasDahl <andreas@hrdahl.dk>
2016-01-03 14:46:48 +0100

    ass1 question2

--- ass1/main/main.py
+++ ass1/main/main.py
@@ -2,12 +2,15 @@ import random
 
 from pycsp.parallel import *
 
+capacities = 10
+distance_to_first = 5
+colours = ['red', 'yellow', 'green']
+
 
 @process
 def producer(chan_out, pause_chan, print_chan):
     ''' Producer and camera
     '''
-    colours = ['red', 'yellow', 'green']
     pauses = 0
     try:
         # Every iteration in this loop is a "tick" for the whole application
@@ -19,13 +22,14 @@ def producer(chan_out, pause_chan, print_chan):
                 pauses = msg
             print_chan("pauses %s" % pauses)
             if pauses <= 0:
-                print_chan("produce somthing")
+                print_chan("produce something")
                 # Produce a ball and send it to the conveyor
                 chan_out(random.choice(colours))
             else:
                 print_chan("wait %s" % pauses)
                 # Do not produce a ball on paused ticks, but decrement the pause counter
                 chan_out(None)
+                print_chan("after wait")
                 pauses -= 1
 
 
@@ -36,11 +40,11 @@ def producer(chan_out, pause_chan, print_chan):
 
 
 @process
-def conveyor(chan_in, red_chan, yellow_chan, green_chan, print_chan):
+def conveyor(chan_in, bins, print_chan):
     ''' Conveyor belt, advances the balls and makes sure they are moved to the
         correct basket
     '''
-    length = 11
+    length = distance_to_first + 3 * len(colours) - 2
     places = [None] * length
 
     try:
@@ -55,16 +59,15 @@ def conveyor(chan_in, red_chan, yellow_chan, green_chan, print_chan):
                 places[i] = places[i-1]
 
             places[0] = chan_in()
-            print_chan("conveyor ball get")
-            if places[4] == "red":
-                red_chan(places[4])
-                places[4] = None
-            if places[7] == "yellow":
-                yellow_chan(places[7])
-                places[7] = None
-            if places[10] == "green":
-                green_chan(places[10])
-                places[10] = None
+            print_chan("conveyor ball get %s" % places[0])
+            next_bin_loc = distance_to_first
+            for i in range(len(bins)):
+                if places[next_bin_loc] == colours[i]:
+                    bins[i](places[next_bin_loc])
+                    places[next_bin_loc] = None
+                else:
+                    bins[i](None)
+                next_bin_loc += 3
 
     except ChannelRetireException:
         print "conveyor: %s" % places
@@ -77,21 +80,22 @@ def conveyor(chan_in, red_chan, yellow_chan, green_chan, print_chan):
 
 @process
 def basket(chan_in, pause_chan, capacity, colour, print_chan):
-    print colour, "basket initialized"
+    printer("%s basket initialized" % colour)
+    empty_delay = -1
     count = 0
     while True:
         ball = chan_in()
         if ball is not None:
             print_chan("basket %s, %s" % (ball, colour))
             if count >= capacity:
-                raise ValueError("BASKET OVERFLOW (%s)" % colour)
+                raise ValueError("BASKET OVERFLOW (%s), empty_delay %s" % (colour, empty_delay))
             if ball != colour:
                 raise ValueError("Ball '%s' ended up in wrong basket (%s)" % (ball, colour))
             count += 1
             if count >= capacity:
                 print_chan("EMPTYING %s, %s" % (ball, colour))
                 count = 0
-                pause_chan(5)  # notify number of ticks the basket require to empty itself
+                pause_chan(5)
     # The basket should not reach this point
     print "BASKET RETIRE"
     retire(chan_in)
@@ -106,17 +110,18 @@ def printer(chan_in):
 if __name__ == '__main__':
     print "run"
     producerChan = Channel("producer")
-    red_chan = Channel("red")
-    yellow_chan = Channel("yellow")
-    green_chan = Channel("green")
-    pauseChan = Channel("pause")
+    pauseChan = Channel("pause", len(colours))
     print_chan = Channel("print")
+    bin_writers = []
+
+    for colour in colours:
+        bin_chan = Channel(colour)
+        Spawn(basket(bin_chan.reader(), pauseChan.writer(), capacities, colour, print_chan.writer()))
+        bin_writers.append(bin_chan.writer())
+
     capacities = 10
     Parallel(
-        basket(red_chan.reader(), pauseChan.writer(), capacities, "red", print_chan.writer()),
-        basket(yellow_chan.reader(), pauseChan.writer(), capacities, "yellow", print_chan.writer()),
-        basket(green_chan.reader(), pauseChan.writer(), capacities, "green", print_chan.writer()),
-        conveyor(producerChan.reader(), red_chan.writer(), yellow_chan.writer(), green_chan.writer(), print_chan.writer()),
+        conveyor(producerChan.reader(), bin_writers, print_chan.writer()),
         producer(producerChan.writer(), pauseChan.reader(), print_chan.writer()),
         printer(print_chan.reader()),
     )
