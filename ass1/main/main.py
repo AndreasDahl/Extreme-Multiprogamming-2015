d034cd32b95666c1684bf33d58102ec07e94a3bb
AndreasDahl <andreas@hrdahl.dk>
2016-01-03 12:34:29 +0100

    question1

--- ass1/main/main.py
+++ ass1/main/main.py
@@ -3,44 +3,49 @@ import random
 from pycsp.parallel import *
 
 
-colours = ['red', 'yellow', 'green']
-
-
 @process
-def producer(chan_out, pause_chan):
+def producer(chan_out, pause_chan, print_chan):
     ''' Producer and camera
     '''
+    colours = ['red', 'yellow', 'green']
     pauses = 0
     try:
         # Every iteration in this loop is a "tick" for the whole application
         while True:
+            print_chan("produce %s" % pauses)
             # Asyncronusly see if the pause channel has any messages
             c, msg = PriSelect(InputGuard(pause_chan), SkipGuard())
-            if msg is not None:
+            if c == pause_chan:
                 pauses = msg
+            print_chan("pauses %s" % pauses)
             if pauses <= 0:
+                print_chan("produce somthing")
                 # Produce a ball and send it to the conveyor
                 chan_out(random.choice(colours))
             else:
+                print_chan("wait %s" % pauses)
                 # Do not produce a ball on paused ticks, but decrement the pause counter
+                chan_out(None)
                 pauses -= 1
+
 
     except ChannelRetireException:
         print "producer stopping"
+    finally:
+        print "Producer ended"
 
 
 @process
-def conveyor(chan_in, basket_chan):
+def conveyor(chan_in, red_chan, yellow_chan, green_chan, print_chan):
     ''' Conveyor belt, advances the balls and makes sure they are moved to the
         correct basket
-
-        TODO: Support multiple baskets
-        TODO: More flexible belt length
     '''
-    places = [None] * 3
+    length = 11
+    places = [None] * length
 
     try:
         while True:
+            print_chan("Conveyor")
             # Move everything one. Can be done smarter, but whatever
             for i, v in reversed(list(enumerate(places))):
                 if i == len(places) - 1 and v is not None:
@@ -50,28 +55,41 @@ def conveyor(chan_in, basket_chan):
                 places[i] = places[i-1]
 
             places[0] = chan_in()
-            basket_chan(places[2])
-            places[2] = None
+            print_chan("conveyor ball get")
+            if places[4] == "red":
+                red_chan(places[4])
+                places[4] = None
+            if places[7] == "yellow":
+                yellow_chan(places[7])
+                places[7] = None
+            if places[10] == "green":
+                green_chan(places[10])
+                places[10] = None
+
     except ChannelRetireException:
         print "conveyor: %s" % places
         retire(chan_in)
         retire(basket_chan)
+    finally:
+        print "conveyor ended"
 
 
 
 @process
-def basket(chan_in, pause_chan, capacity, colour):
+def basket(chan_in, pause_chan, capacity, colour, print_chan):
     print colour, "basket initialized"
     count = 0
     while True:
         ball = chan_in()
         if ball is not None:
-            print "basket", ball
+            print_chan("basket %s, %s" % (ball, colour))
+            if count >= capacity:
+                raise ValueError("BASKET OVERFLOW (%s)" % colour)
             if ball != colour:
-                print "BASKET ERROR"
-                raise ValueError("Ball ended up in wrong basket")
+                raise ValueError("Ball '%s' ended up in wrong basket (%s)" % (ball, colour))
             count += 1
             if count >= capacity:
+                print_chan("EMPTYING %s, %s" % (ball, colour))
                 count = 0
                 pause_chan(5)  # notify number of ticks the basket require to empty itself
     # The basket should not reach this point
@@ -79,24 +97,28 @@ def basket(chan_in, pause_chan, capacity, colour):
     retire(chan_in)
     retire(pause_chan)
 
-
 @process
-def camera(chan_in, chan_out):
-    ball = chan_in()
-    chan_out(ball)
-
+def printer(chan_in):
+    while True:
+        msg = chan_in()
+        print msg
 
 if __name__ == '__main__':
     print "run"
     producerChan = Channel("producer")
-    basketChan = Channel("basket")
+    red_chan = Channel("red")
+    yellow_chan = Channel("yellow")
+    green_chan = Channel("green")
     pauseChan = Channel("pause")
+    print_chan = Channel("print")
     capacities = 10
-
     Parallel(
-        basket(basketChan.reader(), pauseChan.writer(), capacities, "red"),
-        conveyor(producerChan.reader(), basketChan.writer()),
-        producer(producerChan.writer(), pauseChan.reader())
+        basket(red_chan.reader(), pauseChan.writer(), capacities, "red", print_chan.writer()),
+        basket(yellow_chan.reader(), pauseChan.writer(), capacities, "yellow", print_chan.writer()),
+        basket(green_chan.reader(), pauseChan.writer(), capacities, "green", print_chan.writer()),
+        conveyor(producerChan.reader(), red_chan.writer(), yellow_chan.writer(), green_chan.writer(), print_chan.writer()),
+        producer(producerChan.writer(), pauseChan.reader(), print_chan.writer()),
+        printer(print_chan.reader()),
     )
 
     shutdown()
